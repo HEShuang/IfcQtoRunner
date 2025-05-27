7e90a4e685b0456cc6138850b6d5862d1a2d1b12
HEShuang <shuang.pro@gmail.com>
2025-05-27 10:51:55 +0200

    rename lib and folders

--- IfcCore/parse/parse.cmake
+++ IfcCore/parse/parse.cmake
@@ -1,6 +1,6 @@
 set(
     PARSE_SOURCES
-    ${CMAKE_CURRENT_LIST_DIR}/serializer.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/parse.cmake
     ${CMAKE_CURRENT_LIST_DIR}/IfcParseHelper.h
     ${CMAKE_CURRENT_LIST_DIR}/IfcPreview.h
     ${CMAKE_CURRENT_LIST_DIR}/IfcPreview.cpp
